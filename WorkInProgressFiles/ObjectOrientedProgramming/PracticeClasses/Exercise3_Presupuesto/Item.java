267ef9c854f797ff280c677e7e2d5b892dc187ee
Mauro Kein <maurokein1990@gmail.com>
2021-09-13 11:33:26 -0300

    Balanza directory created, Presupuesto directory created, Presupuesto & Item classes created

---
+++ WorkInProgressFiles/ObjectOrientedProgramming/PracticeClasses/Exercise3_Presupuesto/Item.java
@@ -0,0 +1,9 @@
+public class Item extends Object{
+    private String detalle;
+    private Integer cantidad;
+    private Double costoUnitario;
+
+    public double costo(){
+        
+    }
+}
---
+++ WorkInProgressFiles/ObjectOrientedProgramming/PracticeClasses/Exercise3_Presupuesto/Presupuesto.java
@@ -0,0 +1,13 @@
+public class Presupuesto extends Object{
+    private Date fecha;
+    private String cliente;
+
+    public void agregarItem(Item item){
+
+    }
+
+    public double calcularTotal(){
+
+    }
+
+}
